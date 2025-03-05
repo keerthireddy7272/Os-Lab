61d5001a646e07b88e48467b9d0bfb5586707b6b
keerthireddy7272 <kamireddy.cs23@bmsce.ac.in>
2025-03-05 12:22:41 +0530

    Create secondlargest.c

---
+++ secondlargest.c
@@ -0,0 +1,31 @@
+//5. Write a program in C to find the second-largest element in an array.
+
+#include <stdio.h>
+
+void sorting(int arr[], int n){
+for(int i=0;i<n-1;i++){
+for(int j=i+1;j<n;j++){
+if(arr[i]<arr[j]){
+int temp= arr[i];
+arr[i]=arr[j];
+arr[j]=temp;
+}
+}
+}
+}
+int main() {
+int arr[10], n;
+printf("Enter the number of elements: ");
+scanf("%d", &n);
+for (int i = 0; i < n; i++) {
+printf("Enter the %d element: ", i + 1);
+scanf("%d", &arr[i]);
+}
+sorting(arr, n);
+if (n >= 2) {
+printf("The second smallest number is: %d\n", arr[1]);
+}else{
+printf("Array must contain at least two elements to find the second Largest.\n");
+}
+return 0;
+}
