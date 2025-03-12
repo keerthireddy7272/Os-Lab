feb733a5fe3e84a0a44357f39528f412bcd3a28c
keerthireddy7272 <kamireddy.cs23@bmsce.ac.in>
2025-03-12 12:21:48 +0530

    Update FCFS.c

--- FCFS.c
+++ FCFS.c
@@ -1,44 +1,28 @@
 #include <stdio.h>
     int n, i, j, pos, temp, choice, BT[20], WT[20], TAT[20], process[20], total=0;
     float avgtat=0, avgwt=0;
-int SJF()
+int FCFS()
 {
-    for(i=0;i<n;i++)
-    {
-        pos=i;
-        for(j=i+1;j<n;j++)
-        {
-            if(BT[j]<BT[pos])
-                pos=j;
-        }
-        temp=BT[i];
-        BT[i]=BT[pos];
-        BT[pos]=temp;
-
-        temp=process[i];
-        process[i]=process[pos];
-        process[pos]=temp;
-    }
-        WT[0]=0;
+    WT[0]=0;
     for(i=1;i<n;i++)
     {
         WT[i]=0;
         for(j=0;j<i;j++)
             WT[i]+=BT[j];
-        total+=WT[i];
     }
-    avgwt=(float)total/n;
-    total=0;
     printf("\nProcess\t\tBurst Time\t\tWaiting Time\t\tTurnaround Time");
     for(i=0;i<n;i++)
     {
         TAT[i]=BT[i]+WT[i];
-        total+=TAT[i];
-        printf("\nP[%d]\t\t%d\t\t\t%d\t\t\t\t%d",process[i],BT[i],WT[i],TAT[i]);
+        avgwt+=WT[i];
+        avgtat+=TAT[i];
+        printf("\nP[%d]\t\t%d\t\t\t%d\t\t\t\t%d",i+1,BT[i],WT[i],TAT[i]);
     }
-    avgtat=(float)total/n;
-    printf("\n\nAverage Waiting Time=%f",avgwt);
-    printf("\nAverage Turnaround Time=%f\n",avgtat);
+    avgwt =(float)(avgwt)/(float)i;
+    avgtat=(float)(avgtat)/(float)i;
+    printf("\nAverage Waiting Time:%.2f",avgwt);
+    printf("\nAverage Turnaround Time:%.2f\n",avgtat);
+    return 0;
 }
 int main()
 {
@@ -51,6 +35,6 @@ int main()
         scanf("%d",&BT[i]);
         process[i]=i+1;
     }
-    printf("SJF Scheduling\n");
-    SJF();
+    printf("FCFS Scheduling\n");
+    FCFS();
 }
